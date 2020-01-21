71ee28fa83bfecf2f3d02fbfc9eb85e921c01796
abelhii <abel.hii@infinitilab.com>
2020-01-21 18:07:43 +0800

    1) added nominatim service

--- src/app/suggestions/suggestions.page.ts
+++ src/app/suggestions/suggestions.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { GoogleAPIService } from '../services/google-api.service';
 import { Router } from '@angular/router';
 import * as _ from 'lodash';
+import { NominatimAPIService } from '../services/nominatim-api.service';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { FeatureCollection, PropertyGeocoding } from '../models/nominatim';
 
 @Component({
 	selector: 'app-suggestions',
@@ -13,29 +15,35 @@ export class SuggestionsPage implements OnInit {
 	suggestionsList: any[] = [];
 
 	constructor(
-		private _googleApiService: GoogleAPIService,
+		private _nominatimApiService: NominatimAPIService,
+		private geolocation: Geolocation,
 		private router: Router
 	) { }
 
 	ngOnInit() {
-		this._googleApiService.getPlaces().subscribe(places => {
-			console.log(places);
-			this.suggestionsList = this.getPlacePhotoURL(places.results);
+		this.geolocation.getCurrentPosition().then((resp) => {
+			this._nominatimApiService.getPlacesByLatLng(resp.coords.latitude, resp.coords.longitude)
+				.subscribe((result: FeatureCollection) => {
+					this.suggestionsList = _.map(result.features, x => { return x.properties.geocoding });
+					console.log(this.suggestionsList);
+				});
+		}).catch((error) => {
+			console.log('Error getting location', error);
 		});
 	}
 
-	getPlacePhotoURL(placesList: any[]) {
-		_.forEach(placesList, (x, index) => {
-			let photo_ref = x.photos[0].photo_reference;
-			placesList[index].photoURL = this._googleApiService.getPhotoURLByReference(photo_ref);
-				// .subscribe(result => {
-				// 	console.log('photoURL: ', result);
-				// 	placesList[index].photoURL = result;
-				// 	console.log(placesList);
-				// });
-		});
+	// getPlacePhotoURL(placesList: any[]) {
+	// 	_.forEach(placesList, (x, index) => {
+	// 		let photo_ref = x.photos[0].photo_reference;
+	// 		placesList[index].photoURL = this._googleApiService.getPhotoURLByReference(photo_ref);
+	// 		// .subscribe(result => {
+	// 		// 	console.log('photoURL: ', result);
+	// 		// 	placesList[index].photoURL = result;
+	// 		// 	console.log(placesList);
+	// 		// });
+	// 	});
 
-		return placesList;
-	}
+	// 	return placesList;
+	// }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,9 @@
 export const environment = {
   production: false,
   googleApiKey: "AIzaSyD1Ybix0rx1Uw3Cov4yArEuIWKWw5PCXEk",
-  placePhotoUrl: "https://maps.googleapis.com/maps/api/place/photo?"
+  placePhotoUrl: "https://maps.googleapis.com/maps/api/place/photo?",
+  nominatimSearchURL: "https://nominatim.openstreetmap.org/search?",
+  nominatimReverseURL: "https://nominatim.openstreetmap.org/reverse?"
 };
 
 /*
