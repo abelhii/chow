e822ad778f5a3fbdbdae78ea6a4c1618aedfa15c
abelhii <abel.hii@infinitilab.com>
2020-02-06 17:42:53 +0800

    1) included place id to google maps query 2) added chow image component to handle images that are missing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,9 +5,7 @@ import { Component } from '@angular/core';
 	styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-	logoPath: string = "https://ichef.bbci.co.uk/news/410/cpsprodpb/E47E/production/_104449485_b38a5ee8-36b9-4a5a-8e0e-448a5af74ab2.jpg";
-
+	
 	constructor(
 	) {
 
