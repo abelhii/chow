38b5fb8cf890bf194494d33b137de45cae2107fe
abelhii <abel.hii@infinitilab.com>
2020-02-14 12:00:08 +0800

    1) Added misc explanation text to the home screen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import * as _ from 'lodash';
 @Component({
 	selector: 'app-home',
 	templateUrl: 'home.page.html',
 	styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-	
-	constructor(
-	) {
+export class HomePage implements OnInit {
 
+	homeFyi: string[] = [];
+	fyi: string;
+
+	constructor(private http: HttpClient) {
+		if(this.homeFyi.length <= 0){
+			this.getMiscText().subscribe((result) => {
+				this.homeFyi = result.homeFYI;
+				this.fyi = _.sample(result.homeFYI);
+			});
+		}
+	}
+
+	ngOnInit() {
+		this.fyi = _.sample(this.homeFyi);
+	}
+
+	getMiscText(): Observable<any> {
+		return this.http.get("../assets/miscText.json");
 	}
 }
