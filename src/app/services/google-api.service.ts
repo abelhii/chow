d8af903c9846f04669351db88897d5d3d4c93798
abelhii <9299622+abelhii@users.noreply.github.com>
2021-03-23 21:14:36 +0000

    update firebase and chow

--- src/app/services/google-api.service.ts
+++ src/app/services/google-api.service.ts
@@ -1,152 +1,180 @@
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { Observable, BehaviorSubject } from 'rxjs';
-import * as _ from 'lodash';
-import { PlaceFilter, PlaceType } from '../models/filters';
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable, BehaviorSubject } from "rxjs";
+import * as _ from "lodash";
+import { PlaceFilter, PlaceType } from "../models/filters";
 
 @Injectable({
-	providedIn: 'root'
+    providedIn: "root",
 })
 export class GoogleAPIService {
-	apiKey: string = environment.googleApiKey;
-	placePhotoURL: string = environment.placePhotoUrl;
+    apiKey: string = environment.googleApiKey;
+    placePhotoURL: string = environment.placePhotoUrl;
 
-	map: google.maps.Map;
-	placesService: google.maps.places.PlacesService;
-	nearbyPlaces: google.maps.places.PlaceResult[];
+    map: google.maps.Map;
+    placesService: google.maps.places.PlacesService;
+    nearbyPlaces: google.maps.places.PlaceResult[];
 
-	private dataSource = new BehaviorSubject<google.maps.places.PlaceResult[]>(null);
-	data = this.dataSource.asObservable();
+    private dataSource = new BehaviorSubject<google.maps.places.PlaceResult[]>(
+        null
+    );
+    data = this.dataSource.asObservable();
 
-	prevPlaceRequest: google.maps.places.PlaceSearchRequest;
-	filterChanged: boolean = false;
+    prevPlaceRequest: google.maps.places.PlaceSearchRequest;
+    filterChanged: boolean = false;
 
-	constructor(
-		private http: HttpClient
-	) { }
+    constructor(private http: HttpClient) {}
 
-	getPlacesByUserLatLng(lat: number, lng: number, filter: PlaceFilter): Observable<any> {
-		let userLocation = new google.maps.LatLng(lat, lng);
+    getPlacesByUserLatLng(
+        lat: number,
+        lng: number,
+        filter: PlaceFilter
+    ): Observable<any> {
+        let userLocation = new google.maps.LatLng(lat, lng);
 
-		let request: google.maps.places.PlaceSearchRequest = {
-			location: userLocation,
-			radius: filter.Radius || 2500,
-			keyword: filter.Type || PlaceType.Restaurant,
-			type: 'food'
-		};
+        let request: google.maps.places.PlaceSearchRequest = {
+            location: userLocation,
+            radius: filter.Radius || 2500,
+            keyword: filter.Type || PlaceType.Restaurant,
+            type: "food",
+        };
 
-		this.filterChanged = JSON.stringify(this.prevPlaceRequest) !== JSON.stringify(request);
-		let dataSourceLocal = this.dataSource;
+        this.filterChanged =
+            JSON.stringify(this.prevPlaceRequest) !== JSON.stringify(request);
+        let dataSourceLocal = this.dataSource;
 
-		return new Observable<google.maps.places.PlaceResult[]>((observer) => {
-			if (this.nearbyPlaces && this.nearbyPlaces.length > 0 && !this.filterChanged) {
-				// get from local
-				observer.next(this.nearbyPlaces);
-			}
-			else {
-				// get from api
-				this.initialisePlaceService();
-				this.prevPlaceRequest = request;
-				this.placesService.nearbySearch(request, function (results, status) {
-					if (status == google.maps.places.PlacesServiceStatus.OK) {
-						dataSourceLocal.next(results);
-						observer.next(results);
-					} else {
-						observer.error(results);
-					}
-				});
+        return new Observable<google.maps.places.PlaceResult[]>((observer) => {
+            if (
+                this.nearbyPlaces &&
+                this.nearbyPlaces.length > 0 &&
+                !this.filterChanged
+            ) {
+                // get from local
+                observer.next(this.nearbyPlaces);
+            } else {
+                // get from api
+                this.initialisePlaceService();
+                this.prevPlaceRequest = request;
+                this.placesService.nearbySearch(
+                    request,
+                    function (results, status) {
+                        if (
+                            status == google.maps.places.PlacesServiceStatus.OK
+                        ) {
+                            dataSourceLocal.next(results);
+                            observer.next(results);
+                        } else {
+                            observer.error(results);
+                        }
+                    }
+                );
 
-				this.data.subscribe(data => {
-					this.nearbyPlaces = data;
-				});
-			}
-		});
-	}
+                this.data.subscribe((data) => {
+                    this.nearbyPlaces = data;
+                });
+            }
+        });
+    }
 
-	getPlacePhotoUrl(place: google.maps.places.PlaceResult, landscape: boolean): string {
-		let photos = place.photos;
+    getPlacePhotoUrl(
+        place: google.maps.places.PlaceResult,
+        landscape: boolean
+    ): string {
+        let photos = place.photos;
 
-		// prioritize getting landscape photos
-		if (photos && photos.length > 1) {
-			photos = _.filter(photos, x => {
-				return landscape ? x.width > x.height : x.width < x.height;
-			});
-		}
+        // prioritize getting landscape photos
+        if (photos && photos.length > 1) {
+            photos = _.filter(photos, (x) => {
+                return landscape ? x.width > x.height : x.width < x.height;
+            });
+        }
 
-		if (photos && photos[0] != null) {
-			let chosenPhoto = _.sample(photos);
-			if (place.photoUrl) {
-				return place.photoUrl;
-			}
-			if (typeof chosenPhoto.getUrl === "function") {
-				return chosenPhoto.getUrl({ maxHeight: 500, maxWidth: 500 });
-			}
-		} else {
-			return null;
-		}
-	}
+        if (photos && photos[0] != null) {
+            let chosenPhoto = _.sample(photos);
+            if (place.photos[0]) {
+                return place.photos[0].getUrl({});
+            }
+            if (typeof chosenPhoto.getUrl === "function") {
+                return chosenPhoto.getUrl({ maxHeight: 500, maxWidth: 500 });
+            }
+        } else {
+            return null;
+        }
+    }
 
-	openInMaps(place: google.maps.places.PlaceResult) {
-		let location = place.geometry.location;
-		let destination = "";
-		if (typeof location.lat === "function") {
-			destination = place.geometry.location.lat() + "," + place.geometry.location.lng();
-		} else {
-			destination = place.geometry.location.lat + "," + place.geometry.location.lng;
-		}
-		window.open(environment.googleMapsUrl + destination + "&query_place_id=" + place.place_id);
-	}
+    openInMaps(place: google.maps.places.PlaceResult) {
+        let location = place.geometry.location;
+        let destination = "";
+        if (typeof location.lat === "function") {
+            destination =
+                place.geometry.location.lat() +
+                "," +
+                place.geometry.location.lng();
+        } else {
+            destination =
+                place.geometry.location.lat + "," + place.geometry.location.lng;
+        }
+        window.open(
+            environment.googleMapsUrl +
+                destination +
+                "&query_place_id=" +
+                place.place_id
+        );
+    }
 
-	initialisePlaceService() {
-		if (this.placesService == null) {
-			// Initialise places service
-			this.placesService = new google.maps.places.PlacesService(document.createElement('div'));
-		}
-	}
+    initialisePlaceService() {
+        if (this.placesService == null) {
+            // Initialise places service
+            this.placesService = new google.maps.places.PlacesService(
+                document.createElement("div")
+            );
+        }
+    }
 
-	// May come in handy //
+    // May come in handy //
 
-	getPlaceDetailsByPlaceId(placeId: string): Observable<any> {
-		let request: google.maps.places.PlaceDetailsRequest = {
-			placeId: placeId,
-			fields: [
-				'place_id',
-				'plus_code',
-				'name',
-				'address_component',
-				'adr_address',
-				'formatted_address',
-				'photo',
-				'icon',
-				'type',
-				'url']
-		}
-		this.initialisePlaceService();
-		return new Observable<google.maps.places.PlaceResult>((observer) => {
-			this.placesService.getDetails(request, function (place, status) {
-				if (status == google.maps.places.PlacesServiceStatus.OK) {
-					observer.next(place);
-				}
-			});
-		});
-	}
+    getPlaceDetailsByPlaceId(placeId: string): Observable<any> {
+        let request: google.maps.places.PlaceDetailsRequest = {
+            placeId: placeId,
+            fields: [
+                "place_id",
+                "plus_code",
+                "name",
+                "address_component",
+                "adr_address",
+                "formatted_address",
+                "photo",
+                "icon",
+                "type",
+                "url",
+            ],
+        };
+        this.initialisePlaceService();
+        return new Observable<google.maps.places.PlaceResult>((observer) => {
+            this.placesService.getDetails(request, function (place, status) {
+                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                    observer.next(place);
+                }
+            });
+        });
+    }
 
-	getPhotoURLByReference(photo_ref: string) {
-		let headers = {
-			key: this.apiKey,
-			photoreference: photo_ref,
-			maxwidth: '500',
-			maxheight: '500'
-		};
+    getPhotoURLByReference(photo_ref: string) {
+        let headers = {
+            key: this.apiKey,
+            photoreference: photo_ref,
+            maxwidth: "500",
+            maxheight: "500",
+        };
 
-		let url = this.placePhotoURL + new URLSearchParams(headers).toString();
+        let url = this.placePhotoURL + new URLSearchParams(headers).toString();
 
-		return url;
-		// return this.http.get(url);
-	}
+        return url;
+        // return this.http.get(url);
+    }
 
-	getSamplePlaces(): Observable<any> {
-		return this.http.get("./assets/samplePlacesData.json");
-	}
+    getSamplePlaces(): Observable<any> {
+        return this.http.get("./assets/samplePlacesData.json");
+    }
 }
--- src/app/services/place-storage.service.ts
+++ src/app/services/place-storage.service.ts
@@ -1,59 +1,66 @@
-import { Injectable } from '@angular/core';
-import { Storage } from '@ionic/storage';
-import { Observable } from 'rxjs/internal/Observable';
+import { Injectable } from "@angular/core";
+import { Storage } from "@ionic/storage";
+import { Observable } from "rxjs/internal/Observable";
 
 @Injectable({
-	providedIn: 'root'
+    providedIn: "root",
 })
 export class PlaceStorageService {
+    constructor(private storage: Storage) {}
 
-	constructor(private storage: Storage) { }
+    getPlaces(): Promise<any> {
+        return this.storage.get("places");
+    }
 
-	getPlaces(): Promise<any> {
-		return this.storage.get("places");
-	}
+    getPlaceById(placeId: string) {
+        let places: google.maps.places.PlaceResult[];
+        return new Observable<number>((observer) => {
+            this.getPlaces()
+                .then((result) => {
+                    places = JSON.parse(result) || [];
+                })
+                .finally(() => {
+                    observer.next(places.findIndex((x) => x.id == placeId));
+                });
+        });
+    }
 
-	getPlaceById(placeId: string) {
-		let places: google.maps.places.PlaceResult[];
-		return new Observable<number>((observer) => {
-			this.getPlaces().then((result) => {
-				places = JSON.parse(result) || [];
-			}).finally(() => {
-				observer.next(places.findIndex(x => x.id == placeId));
-			})
-		});
-	}
+    getPlacesList(): Observable<google.maps.places.PlaceResult[]> {
+        return new Observable<google.maps.places.PlaceResult[]>((observer) => {
+            this.getPlaces().then((result) => {
+                observer.next(JSON.parse(result));
+            });
+        });
+    }
 
-	getPlacesList(): Observable<google.maps.places.PlaceResult[]> {
-		return new Observable<google.maps.places.PlaceResult[]>((observer) => {
-			this.getPlaces().then((result) => {
-				observer.next(JSON.parse(result));
-			});
-		})
-	}
+    setPlace(place: any, photoUrl: string) {
+        let placesList: google.maps.places.PlaceResult[];
+        let isSaved: boolean = false;
 
-	setPlace(place: google.maps.places.PlaceResult, photoUrl: string) {
-		let placesList: google.maps.places.PlaceResult[];
-		let isSaved: boolean = false;
+        return new Observable<boolean>((observer) => {
+            this.getPlaces()
+                .then((result) => {
+                    placesList = JSON.parse(result) || [];
+                    place.photos.push(photoUrl);
 
-		return new Observable<boolean>((observer) => {
-			this.getPlaces().then((result) => {
-				placesList = JSON.parse(result) || [];
-				place.photoUrl = photoUrl;
-
-				if (!placesList.find(x => x.id == place.id)) {
-					placesList = placesList.concat(place);
-					isSaved = true;
-				} else {
-					let placeIndex = placesList.findIndex(x => x.id == place.id);
-					placesList.splice(placeIndex, 1);
-					isSaved = false;
-				}
-			}).finally(() => {
-				this.storage.set("places", JSON.stringify(placesList)).then(() => {
-					observer.next(isSaved);
-				});
-			});
-		})
-	}
+                    if (!placesList.find((x) => x.id == place.id)) {
+                        placesList = placesList.concat(place);
+                        isSaved = true;
+                    } else {
+                        let placeIndex = placesList.findIndex(
+                            (x) => x.id == place.id
+                        );
+                        placesList.splice(placeIndex, 1);
+                        isSaved = false;
+                    }
+                })
+                .finally(() => {
+                    this.storage
+                        .set("places", JSON.stringify(placesList))
+                        .then(() => {
+                            observer.next(isSaved);
+                        });
+                });
+        });
+    }
 }
